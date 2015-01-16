ffe586f3fbb3548ce70cf5f55c41fdf0d312fd14
Applemoon <sanyauvarov@mail.ru>
2015-01-16 22:36:29 +0300

    Выводится список задач

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from todo_app.models import Task
 
-# Create your views here.
+
+def index(request):
+    tasks_list = Task.objects.order_by('-due_date')
+    context = {'tasks_list': tasks_list}
+    return render(request, 'todo_app/index.html', context)
