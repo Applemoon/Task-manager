3d8e5deb5aca6eb0e7c0bf94d18aaed0b1307fb5
Applemoon <sanyauvarov@mail.ru>
2015-01-19 01:09:38 +0300

    Настройка tools/wsgi.py

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -8,7 +8,23 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+virtenv = os.environ['APPDIR'] + '/virtenv/'
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except:
+    pass
+ 
+# new codes we adding for Django
+import sys
+import django.core.handlers.wsgi
+ 
+os.environ['DJANGO_SETTINGS_MODULE'] = os.environ['OPENSHIFT_APP_NAME']+'.settings'
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', os.environ['OPENSHIFT_APP_NAME']))
+application = django.core.handlers.wsgi.WSGIHandler()
