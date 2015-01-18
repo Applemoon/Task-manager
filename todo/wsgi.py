a0ba9ae5d9ed093d30e3330e6ae2b90b6bdbf53e
Applemoon <sanyauvarov@mail.ru>
2015-01-19 01:43:06 +0300

    Обновление tools/wsgi.py

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -7,13 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+import os, sys
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
 
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
-virtenv = os.environ['APPDIR'] + '/virtenv/'
+
+
+# virtenv = os.environ['APPDIR'] + '/virtenv/'
+virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
 os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
@@ -22,7 +26,6 @@ except:
     pass
  
 # new codes we adding for Django
-import sys
 import django.core.handlers.wsgi
  
 os.environ['DJANGO_SETTINGS_MODULE'] = os.environ['OPENSHIFT_APP_NAME']+'.settings'
